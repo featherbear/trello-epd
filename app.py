8fef101e2a85fd01186d617e7c9524eca130027c
Andrew Wong <andrew.j.wong@outlook.com>
2019-12-11 14:00:50 +1100

    Refactor Trello component into trelloConnector.py
    Change list_cards() to list_cards_iter() in favour of generator performance

--- app.py
+++ app.py
@@ -1,25 +1,7 @@
-from trello import TrelloClient
-import os
-from dotenv import load_dotenv
-load_dotenv()
+import trelloConnector
 
-# Create the client connection
-client = TrelloClient(
-    api_key=os.getenv("TRELLO_API_KEY"),
-    api_secret=os.getenv("TRELLO_API_TOKEN")
-)
-
-# Find / create board
-targetBoard = next(filter(lambda b: b.name == os.getenv("BOARD_NAME"), client.list_boards()), None)
-if targetBoard is None:
-    targetBoard = client.add_board(os.getenv("BOARD_NAME"))
-
-# Find / create list
-targetList = next(filter(lambda l: l.name == os.getenv("LIST_NAME"), targetBoard.list_lists()), None)
-if targetList is None:
-    targetList = targetBoard.add_list(os.getenv("LIST_NAME"))
-
-for card in targetList.list_cards():
+cards = trelloConnector.fetch()
+for card in cards:
     print(f"Card: {card.name}\nDescription: {card.description}\n")
     # for label in card.labels:
     #   label.color
